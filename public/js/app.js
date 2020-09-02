13e382f38b528acee1de9acea31801a73ba0ca32
Whiskey <whiskey@santema.eu>
2020-09-02 23:49:43 +0200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,3 @@
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log('Error: ' + data.error)
-        } else {
-            console.log(data);
-        }
-    })
-})
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1')
@@ -24,7 +14,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'loading...';
     messageTwo.textContent = '';
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = 'Error: ' + data.error;
